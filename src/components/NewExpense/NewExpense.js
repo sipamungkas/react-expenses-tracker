f8ec4739091bac78d7d211a79c1bcdfebaa160e5
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-06-28 20:05:07 +0700

    feat: add full implementation expenses tracker

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const hideFormHandler = () => [setShowForm(false)];
+
+  return (
+    <div className="new-expense">
+      {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onHideForm={hideFormHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
